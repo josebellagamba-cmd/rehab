3e19d9d1466bbd5bdf8beb870e47f1b44c45708f
josebellagamba-cmd <josebellagamba@gmail.com>
2026-06-19 10:12:49 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'rehab-v2';
+const ASSETS = [
+  '/rehab/',
+  '/rehab/index.html',
+  '/rehab/manifest.json',
+  '/rehab/icon-192.png',
+  '/rehab/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/rehab/index.html')))
+  );
+});
